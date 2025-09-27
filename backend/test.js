de8d5afe947ccf95ccfe29cf578dc3eadeae48f7
Phakaphol Dherachaisuphakij <godzk25@gmail.com>
2025-09-28 03:38:08 +0700

    fix a backend

--- backend/test.js
+++ backend/test.js
@@ -105,7 +105,7 @@ app.post("/api/login", async (req, res) => {
 
 // Get all users (manager only)  , auth(["manager"])
 app.get("/api/users", async (req, res) => {
-  const { data, error } = await supabase.from("users").select("id, username, avatar, role, firstname, lastname, tel , rule",);
+  const { data, error } = await supabase.from("users").select("id, username, avatar, role");
   if (error) return res.status(500).json({ error: error.message });
   res.json(data);
 });
